e957f33ab2103a8bd4fc2d08ff0648e820d114b3
George Alexandria <giorgi.alexandria@gmail.com>
2016-07-21 23:51:09 +0300

    Added singleton ThreadPool class

---
+++ TreadPoolImplementation/TreadPoolImplementation/ThreadPool.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TreadPoolImplementation
+{
+  public sealed class ThreadPool
+  {
+    private static readonly ThreadPool instance = new ThreadPool();
+
+    public static ThreadPool Instance
+    {
+      get
+      {
+        return instance;
+      }
+    }
+
+    private ThreadPool()
+    {
+    }
+  }
+}
