b0a1e63d2fc7cba5b66cbeede3ec6386db1ee8bb
George Alexandria <giorgi.alexandria@gmail.com>
2016-07-23 03:45:13 +0300

    Added the thread safe access to first and last element in local queue of worker

--- TreadPoolImplementation/TreadPoolImplementation/ThreadPool.cs
+++ TreadPoolImplementation/TreadPoolImplementation/ThreadPool.cs
@@ -2,11 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace TreadPoolImplementation
 {
   public sealed class ThreadPool
   {
+    private readonly LinkedList<Action> tasks = new LinkedList<Action>();
+    private readonly List<Worker> workers;
+
+    private object tasksLock = new object();
+    private object criticalLock = new object();
+
     private static readonly ThreadPool instance = new ThreadPool();
 
     public static ThreadPool Instance
@@ -20,5 +27,14 @@ namespace TreadPoolImplementation
     private ThreadPool()
     {
     }
+
+    public void QueueUserWorkTask(Action task)
+    {
+      lock (tasksLock)
+      {
+        tasks.AddLast(task);
+      }
+    }
+
   }
 }
