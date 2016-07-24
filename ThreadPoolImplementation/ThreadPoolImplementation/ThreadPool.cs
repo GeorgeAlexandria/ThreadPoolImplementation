866930b5a82b0c9d81cb9884ebfdf0abd4e0a2c1
George Alexandria <giorgi.alexandria@gmail.com>
2016-07-25 00:17:46 +0300

    Code clean up (remove unused).

--- ThreadPoolImplementation/ThreadPoolImplementation/ThreadPool.cs
+++ ThreadPoolImplementation/ThreadPoolImplementation/ThreadPool.cs
@@ -12,79 +12,12 @@ namespace ThreadPoolImplementation
     private class Worker
     {
       public Thread thread;
-      // This list always must contains a one empty node with null action
-      public readonly LinkedList<Action> tasks = new LinkedList<Action>();
 
       private ILogger logger;
 
-      private string name;
-      public Action FirstTask
-      {
-        get
-        {
-          Action result = null;
-          if (Monitor.TryEnter(tasks.First))
-          {
-            try
-            {
-              result = tasks.First.Value;
-              if (result != null)
-              {
-                tasks.RemoveFirst();
-              }
-            }
-            finally
-            {
-              Monitor.Exit(tasks.First);
-            }
-          }
-          return result;
-        }
-      }
-
-      private Action LastTask
-      {
-        get
-        {
-          Action result = null;
-          if (Monitor.TryEnter(tasks.Last))
-          {
-            try
-            {
-              result = tasks.Last.Value;
-              if (result != null)
-              {
-                tasks.RemoveLast();
-              }
-            }
-            finally
-            {
-              Monitor.Exit(tasks.Last);
-            }
-          }
-          return result;
-        }
-      }
-
-      public void AddTask(Action task)
-      {
-        lock (tasks.Last)
-        {
-          tasks.AddLast(task);
-        }
-      }
-
-      public Worker(ILogger logger, string name)
+      public Worker(ILogger logger)
       {
         this.logger = logger;
-        tasks.AddFirst(new LinkedListNode<Action>(null));
-      }
-
-      public Worker(Action task, string name)
-      {
-        tasks.AddFirst(new LinkedListNode<Action>(null));
-        tasks.AddLast(task);
-        this.name = name;
       }
 
       public void Start()
@@ -152,7 +85,7 @@ namespace ThreadPoolImplementation
       workers = new List<Worker>(countWorkers);
       for (int i = 0; i < countWorkers; i++)
       {
-        Worker worker = new Worker(new ConsoleLogger(), $"{i}");
+        Worker worker = new Worker(new ConsoleLogger());
         worker.Start();
         workers.Add(worker);
       }
